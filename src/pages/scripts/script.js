4f9fb1cd0eb7687427ce88f903761934a76de8ab
walidelkad <walid.el.kaddouri@student.howest.be>
2025-10-26 22:39:21 +0100

    added some first gsap animation

--- src/pages/scripts/script.js
+++ src/pages/scripts/script.js
@@ -1,35 +1,39 @@
-const init = () => {
-  //
-  gsap.registerPlugin(ScrollTrigger);
-  /*
-    gsap.to(".square", {
-      x: 700,
-      duration: 3,
-      scrollTrigger: {
-        trigger: ".square",
-        start: "top 70%",
-        end: "bottom 50%",
-        toggleClass: "white",
-        markers: { fontSize: "25px", fontWeight: "bold" },
-        scrub: 0.5, //true
-        pin: true,
-      },
-    });*/
+import gsap from "gsap-trial";
+import { SplitText } from "gsap-trial/SplitText";
+import { ScrollTrigger } from "gsap-trial/ScrollTrigger";
 
-  gsap.to(".square", {
-    //x: 700,
-    duration: 3,
-    scrollTrigger: {
-      trigger: ".square2",
-      start: "top 80%",
-      end: "top 30%",
-      toggleClass: "white",
-      markers: { fontSize: "25px", fontWeight: "bold" },
-      scrub: true,
-      pin: ".square",
-      pinSpacing: false, //set to true to see extra padding added
-    },
-  });
 
-};
-init();
+gsap.registerPlugin(SplitText, ScrollTrigger)
+
+let mySplitText = new SplitText(' .header__title', {type: 'chars'});
+let mySplitText2 = new SplitText(' .works__title', {type: 'chars'});
+
+let chars = mySplitText.chars;
+let chars2 = mySplitText2.chars;
+
+
+gsap.from(chars, {
+    yPercent: 130,
+    stagger: 0.02,
+    ease: "back.out",
+    duration: 1,
+    ScrollTrigger: {
+        trigger: '.header__title',
+        start: "top 80%",
+        markers: true
+    }
+})
+
+
+gsap.from(chars2, {
+    yPercent: 130,
+    stagger: 0.02,
+    ease: "back.out",
+    duration: 1,
+    ScrollTrigger: {
+        trigger: '.works',
+        start: "top 10%",
+        markers: true
+    }
+})
+
---
+++ src/pages/scripts/scriptest.js
@@ -0,0 +1,39 @@
+const init = () => {
+    /*
+  //
+  gsap.registerPlugin(ScrollTrigger);
+  /*
+    gsap.to(".square", {
+      x: 700,
+      duration: 3,
+      scrollTrigger: {
+        trigger: ".square",
+        start: "top 70%",
+        end: "bottom 50%",
+        toggleClass: "white",
+        markers: { fontSize: "25px", fontWeight: "bold" },
+        scrub: 0.5, //true
+        pin: true,
+      },
+    });
+
+  gsap.to(".square", {
+    //x: 700,
+    duration: 3,
+    scrollTrigger: {
+      trigger: ".square2",
+      start: "top 80%",
+      end: "top 30%",
+      toggleClass: "white",
+      markers: { fontSize: "25px", fontWeight: "bold" },
+      scrub: true,
+      pin: ".square",
+      pinSpacing: false, //set to true to see extra padding added
+    },
+  });
+*/
+};
+import gsap from "gsap-trial";
+
+
+init();
